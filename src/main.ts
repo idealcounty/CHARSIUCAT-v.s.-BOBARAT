e642f2654eb9ff546c5768b3cde9e012f3df627c
Suzushima-Arisu <144450332+Suzushima-Arisu@users.noreply.github.com>
2025-04-11 16:03:49 +0800

    添加Motiva Sans字体

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import App from './App.vue'
 import axios from 'axios'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './assets/global.css'
 
 //设置后端地址（本地或服务器），会将请求转发到后端端口
 axios.defaults.baseURL = ("http://localhost:8080")

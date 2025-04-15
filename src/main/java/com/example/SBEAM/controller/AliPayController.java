9de8c6332deacd2a3b8cf0fa0f574ed45695b20a
idealCounty <1299351533@qq.com>
2025-04-15 17:11:45 +0800

    支付相关更新

---
+++ src/main/java/com/example/SBEAM/controller/AliPayController.java
@@ -0,0 +1,10 @@
+package com.example.SBEAM.controller;
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+
+public class AliPayController {
+
+}

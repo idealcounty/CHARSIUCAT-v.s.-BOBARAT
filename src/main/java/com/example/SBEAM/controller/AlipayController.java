449bbaedcb1e176af9e7563310ac3c66acd7f8e2
idealCounty <1299351533@qq.com>
2025-04-16 16:58:02 +0800

    删去初始商品

--- src/main/java/com/example/SBEAM/controller/AlipayController.java
+++ src/main/java/com/example/SBEAM/controller/AlipayController.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 @RestController
-@RequestMapping("/alipay")
+@RequestMapping("/api/alipay")
 public class AlipayController {
 
     @Autowired
